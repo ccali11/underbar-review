bbadf0d3df1e7b0fb235411b8a472a958ba6dfe9
Chris Cali <ChristopherCali1415@gmail.com>
2020-08-18 14:25:17 -0400

    Implement _.each and fix its corresponding test suite

--- src/underbar.js
+++ src/underbar.js
@@ -50,6 +50,17 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    // If collection is an array, use regular for loop to iterate over collection and call iterator accordingly
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+      // If collection is an object, use a for in loop
+    } else if (typeof collection === 'object') {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
